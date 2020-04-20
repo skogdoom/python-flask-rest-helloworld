8f3e5b7ef052506c1f7fcca41292b24ab85f30a5
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-04-20 23:19:05 +0200

    Added config file

--- src/application.py
+++ src/application.py
@@ -1,5 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else if app.config["ENV"] == "testing":
+    app.config.from_object("config.TestingConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
 
 @app.route('/')
 def hello():
