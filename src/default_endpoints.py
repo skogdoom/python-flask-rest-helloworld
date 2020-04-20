361919de71c014fc8351da87edf6fbaadca48b84
David Andreasson <647640+skogdoom@users.noreply.github.com>
2020-04-21 00:17:37 +0200

    request param

--- src/default_endpoints.py
+++ src/default_endpoints.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 default_endpoints = Blueprint('default_endpoints', __name__)
 
 @default_endpoints.route('/')
 def index():
-    return jsonify({'greeting':'Hello World!'})
+    name_param = request.args.get('name')
+
+    if name_param is None:
+        return jsonify({'greeting':'Hello World!'})
+    else:
+        return jsonify({'greeting':'Hello {0}'.format(name_param)})
